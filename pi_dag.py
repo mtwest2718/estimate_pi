5fbc1318c837b4e6423a5d320dfa13a88095b5c6
WestByNoreaster <mtwest2718@protonmail.com>
2020-11-25 20:33:38 +0200

    rename rewrite to pi_dag
    a git move so as to get rid of old version of the dag script.

--- pi_dag.py
+++ pi_dag.py
@@ -5,10 +5,10 @@ from random import sample, seed
 from pathlib import Path
 from os import mkdir
 from os.path import isdir, join
+import pdb
 # HTCondor libraries
 import htcondor
 from htcondor import dags
-from htcondor.personal import PersonalPool
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -30,7 +30,7 @@ if __name__ == "__main__":
 
     ## Set up directory structure for job log, out and error
     this_dir = Path(__file__).parent
-    for dir in ['log', 'out', 'err', 'results']:
+    for dir in ['log', 'results']:
         new_dir = join(this_dir, dir)
         if not isdir(new_dir):
             mkdir(new_dir)
@@ -41,12 +41,13 @@ if __name__ == "__main__":
     # Define the Sampling Jobs (submit file)
     sample_sub = htcondor.Submit(
         executable = 'pi_samples.py',
-        arguments = '--seed $(seed) --iters $(iters) --threads $(threads) --outfile samples_$(ProcID).csv',
+        arguments = '--seed $(seed) --iters $(iters) --threads $(threads) --outfile $(outfile)',
         should_transfer_files = "YES",
-        initialdir = "results",
+        transfer_output_files = "$(outfile)",
+        initialdir = 'results',
         log = '../log/samples.log',
-        output = '../out/samples_$(ProcID).out',
-        error = '../err/samples_$(ProcID).err',
+        output = '../log/samples.out',
+        error = '../log/samples.err',
         request_cpus = '$(threads)',
         request_memory = '1GB',
         request_disk = '500MB',
@@ -56,8 +57,11 @@ if __name__ == "__main__":
     seed_nums = sample(range(1000000), k=args.njobs)
     # construct input arg dicts for sampling jobs
     sample_vars = []
-    for N in seed_nums:
-        sample_vars.append({'seed': str(N), 'iters': str(args.iters), 'threads': str(args.threads)})
+    for i in range(args.njobs):
+        sample_vars.append(
+            {'seed': str(seed_nums[i]), 'iters': str(args.iters), 'threads': str(args.threads),
+            'outfile': 'samples_{}.csv'.format(i)}
+        )
     # Add sampling jobs layer to DAG
     sample_layer = pi_dag.layer(
         name = 'sample', submit_description = sample_sub,
@@ -65,23 +69,23 @@ if __name__ == "__main__":
     )
 
     # Define the Trace plot Jobs (submit file)
+    files_list = ' '.join(f"samples_{j}.csv" for j in range(args.njobs))
     trace_sub = htcondor.Submit(
         executable = 'pi_trace.py',
-        arguments = '--infiles $(files_list) --estimator $(est_type)',
+        arguments = '--infiles $(infiles) --estimator $(est_type)',
         should_transfer_files = "YES",
         transfer_input_files = 'results/',
         log = 'log/trace.log',
-        output = 'out/trace_$(est_type).out',
-        error = 'err/trace_$(est_type).err',
+        output = 'log/trace.out',
+        error = 'log/trace.err',
         request_cpus = '1',
         request_memory = '1GB',
         request_disk = '3GB',
     )
     # construct input arg dicts for trace plotting jobs
     trace_vars = []
-    files_list = ' '.join(f"samples_{j}.csv" for j in range(args.njobs))
     for est_type in ['area', 'func']:
-        trace_vars.append({'files_list': files_list, 'est_type': est_type})
+        trace_vars.append({'infiles': files_list, 'est_type': est_type})
     # add the summary job layer to DAG
     trace_layer = sample_layer.child_layer(
         name = 'trace', submit_description = trace_sub,
@@ -90,17 +94,15 @@ if __name__ == "__main__":
 
     # Write DAG file to disk
     dag_file = dags.write_dag(pi_dag, this_dir, dag_file_name='pi.dag')
-    
+
     if args.submit:
         # Generate condor_submit file for DAG
         dag_submit = htcondor.Submit.from_dag(
             str(dag_file),
             {'force': 1, 'batch-name': 'MmmmmPi'}
         )
-		
+
         schedd = htcondor.Schedd()
         # Connect to the Scheduler and submit the DAGman job
         with schedd.transaction() as txn:
             cluster_id = dag_submit.queue(txn)
-
-
