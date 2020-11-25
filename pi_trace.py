ec6c46b20cd3f1d36fa2252b313cf6acda2af8ab
WestByNoreaster <mtwest2718@protonmail.com>
2020-11-25 15:46:04 +0200

    Create pi_trace.py
    python script to generate trace plots of pi estimators

---
+++ pi_trace.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import argparse
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import pdb
+import os
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--infiles', nargs='+', help="list of input files")
+    parser.add_argument('--estimator', dest='est', choices=['area', 'func'],
+        help="Which estimator type to plot") 
+    args = parser.parse_args()
+
+    fig, ax = plt.subplots()
+    ax.set(xlabel='Samples (#)', ylabel='Estimate')
+    ax.set(title=r'Stochastic estimate of $\pi$ using {}'.format(args.est.upper()))
+    ax.grid()
+
+    col = 'est_' + args.est
+    for m, csvfile in enumerate(args.infiles):
+        df = pd.read_csv(csvfile, usecols=[col])
+        df.rename(columns={col: 'estimate'}, inplace=True)
+        # subset of estimates where #-samples is an power of 2
+        sample_idx = 2**np.arange(0, np.floor(np.log2(len(df)))+1)
+        subset_df = df.iloc[sample_idx-1]
+
+        # plot trace with semilog-X axis in base 10
+        ax.semilogx(sample_idx, subset_df.estimate, '.-', c='blue', alpha=0.5)
+
+    ax.set_yticks(np.arange(0,1.5,0.25)*np.pi)
+    ax.set_yticklabels(
+        ['0', r'$\pi/4$', r'$\pi/2$', r'$3\pi/4$', r'$\pi$', r'$5\pi/4$']
+    )
+
+    fig.savefig('pi_trace_{}.png'.format(args.est))
