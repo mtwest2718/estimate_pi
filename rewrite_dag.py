8aa70919ede5518d1b9bfc19b5bdf4f6889628c9
WestByNoreaster <mtwest2718@protonmail.com>
2020-11-25 19:29:09 +0200

    just sample jobs in rewrite
    included the rest of the DAG structure for just running sampling jobs in the rewrite of the DAG script. Still haven't checked it yet.

--- rewrite_dag.py
+++ rewrite_dag.py
@@ -30,3 +30,46 @@ if __name__ == "__main__":
 
     ## Set up directory structure for job log, out and error
     this_dir = Path(__file__).parent
+
+    ## Making the DAG
+    pi_dag = dags.DAG()
+
+    sample_sub = htcondor.Submit(
+        executable = 'pi_samples.py',
+        arguments = '--seed $(seed) --iters $(iters) --threads $(threads) --outfile samples_$(ProcId).csv',
+        should_transfer_files = "YES",
+        initialdir = 'results',
+        log = '../log/samples.log',
+        output = '../out/samples_$(ProcId).out',
+        error = '../err/samples_$(ProcId).err',
+        request_cpus = '$(threads)',
+        request_memory = '1GB',
+        request_disk = '500MB',
+    )
+    # root RNG seed
+    seed(seed_num)
+    seed_nums = sample(range(1000000), k=njobs)
+    # construct input arg dicts for sampling jobs
+    sample_vars = []
+    for i in seed_nums:
+        sample_vars.append({'seed': str(i), 'iters': str(iters), 'threads': str(threads)}
+    # Add sampling jobs layer to DAG
+    sample_layer = pi_dag.layer(
+        name = 'sample', submit_description = sample_sub,
+        vars = sample_vars
+    )
+
+    # Write DAG file to disk
+    dag_file = dags.write_dag(pi_dag, this_dir, dag_file_name='pi.dag')
+
+    if args.submit:
+        # Generate condor_submit file for DAG
+        dag_submit = htcondor.Submit.from_dag(
+            str(dag_file),
+            {'force': 1, 'batch-name': 'MmmmmPi'}
+        )
+
+        schedd = htcondor.Schedd()
+        # Connect to the Scheduler and submit the DAGman job
+        with schedd.transaction() as txn:
+            cluster_id = dag_submit.queue(txn)
