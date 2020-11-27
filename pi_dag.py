227c66110b4cb491006114a5b8f97bc964493b0b
WestByNoreaster <mtwest2718@protonmail.com>
2020-11-27 17:10:05 +0200

    made functions for submit files
    Moved the functionality of making the submit objects and the macro dictionary lists into functions. Partly just to clean up __main__

--- pi_dag.py
+++ pi_dag.py
@@ -10,6 +10,57 @@ import pdb
 import htcondor
 from htcondor import dags
 
+def sampling_jobs(rng_seed, njobs, iters, threads):
+    # Define the Sampling Jobs (submit file)
+    sample_sub = htcondor.Submit(
+        executable = 'pi_samples.py',
+        arguments = '--seed $(seed) --iters $(iters) --threads $(threads) --outfile $(outfile)',
+        should_transfer_files = "YES",
+        initialdir = 'results',
+        log = '../log/samples.log',
+        output = '../log/samples_$(id).out',
+        error = '../log/samples_$(id).err',
+        request_cpus = '$(threads)',
+        request_memory = '1GB',
+        request_disk = '500MB',
+    )
+    # root RNG seed
+    seed(rng_seed)
+    seed_nums = sample(range(1000000), k=njobs)
+    # construct input arg dicts for sampling jobs
+    sample_vars = []
+    for i in range(njobs):
+        sample_vars.append(
+            {'seed': str(seed_nums[i]), 'iters': str(iters), 'threads': str(threads),
+            'outfile': 'samples_{}.csv'.format(i), "id": str(i)}
+        )
+    return sample_sub, sample_vars
+
+def trace_plot_jobs(njobs, threads):
+    # Define the Trace plot Jobs (submit file)
+    trace_sub = htcondor.Submit(
+        executable = 'pi_trace.py',
+        arguments = '--infiles $(infiles) --estimator $(est_type)',
+        should_transfer_files = "YES",
+        transfer_input_files = 'results/',
+        log = 'log/trace.log',
+        output = 'log/trace_$(est_type).out',
+        error = 'log/trace_$(est_type).err',
+        request_cpus = '1',
+        request_memory = '1GB',
+        request_disk = '3GB',
+    )
+    # list of input files for trace plots
+    files_list = [f"samples_{j}.csv" for j in range(args.njobs)]
+    if args.threads > 1:
+        files_list = [f.replace('.', f"_{i}.") for i in range(args.threads) for f in files_list]
+    # construct input arg dicts for trace plotting jobs
+    trace_vars = []
+    for est_type in ['area', 'func']:
+        trace_vars.append({'infiles': ' '.join(files_list), 'est_type': est_type})
+    return trace_sub, trace_vars
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
@@ -38,57 +89,17 @@ if __name__ == "__main__":
     ## Making the DAG
     pi_dag = dags.DAG()
 
-    # Define the Sampling Jobs (submit file)
-    sample_sub = htcondor.Submit(
-        executable = 'pi_samples.py',
-        arguments = '--seed $(seed) --iters $(iters) --threads $(threads) --outfile $(outfile)',
-        should_transfer_files = "YES",
-        initialdir = 'results',
-        log = '../log/samples.log',
-        output = '../log/samples_$(id).out',
-        error = '../log/samples_$(id).err',
-        request_cpus = '$(threads)',
-        request_memory = '1GB',
-        request_disk = '500MB',
-    )
-    # root RNG seed
-    seed(args.seed)
-    seed_nums = sample(range(1000000), k=args.njobs)
-    # construct input arg dicts for sampling jobs
-    sample_vars = []
-    for i in range(args.njobs):
-        sample_vars.append(
-            {'seed': str(seed_nums[i]), 'iters': str(args.iters), 'threads': str(args.threads),
-            'outfile': 'samples_{}.csv'.format(i), "id": str(i)}
-        )
     # Add sampling jobs layer to DAG
+    sample_sub, sample_vars = sampling_jobs(
+        args.seed, args.njobs, args.iters, args.threads
+    )
     sample_layer = pi_dag.layer(
         name = 'sample', submit_description = sample_sub,
         vars = sample_vars
     )
 
-    # Define the Trace plot Jobs (submit file)
-    trace_sub = htcondor.Submit(
-        executable = 'pi_trace.py',
-        arguments = '--infiles $(infiles) --estimator $(est_type)',
-        should_transfer_files = "YES",
-        transfer_input_files = 'results/',
-        log = 'log/trace.log',
-        output = 'log/trace_$(est_type).out',
-        error = 'log/trace_$(est_type).err',
-        request_cpus = '1',
-        request_memory = '1GB',
-        request_disk = '3GB',
-    )
-    # list of input files for trace plots
-    files_list = [f"samples_{j}.csv" for j in range(args.njobs)]
-    if args.threads > 1:
-        files_list = [f.replace('.', f"_{i}.") for i in range(args.threads) for f in files_list]
-    # construct input arg dicts for trace plotting jobs
-    trace_vars = []
-    for est_type in ['area', 'func']:
-        trace_vars.append({'infiles': ' '.join(files_list), 'est_type': est_type})
     # add the summary job layer to DAG
+    trace_sub, trace_vars = trace_plot_jobs(args.njobs, args.threads)
     trace_layer = sample_layer.child_layer(
         name = 'trace', submit_description = trace_sub,
         vars = trace_vars
