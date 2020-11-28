bca7c9ad5186cb62f1cbc6a5ccb5aa842f4cef15
WestByNoreaster <mtwest2718@protonmail.com>
2020-11-28 23:41:12 +0200

    Update execute_machines.py
    - create classads list so that results printed in particular order
    - add kernel info
    - restrict machine ads to just execute nodes with TargetType==Job constraint

--- execute_machines.py
+++ execute_machines.py
@@ -4,25 +4,27 @@ import pandas as pd
 # HTCondor libraries
 import htcondor
 
+# Desired classad keys for Execute Machine nodes
+classad_keys = [
+    'Name', 'Memory', 'Disk', 'VirtualMemory',
+    'Cpus', 'Arch', 'CpuFamily', 'CpuModelNumber',
+    'OpSysLegacy', 'OpSysLongName', 'UtsnameRelease', 'UtsnameVersion',
+    'TotalSlots', 'SlotType', 'CondorVersion',
+    'HasMPI', 'HasFileTransferPluginMethods', "HasPerFileEncryption",
+    'LoadAvg', 'CondorLoadAvg', 'State', 'Activity',
+    'DaemonStartTime', 'LastHeardFrom'
+]
+
 coll = htcondor.Collector()
-# Get information about all machines listed in Collector
+# Get information about all execute machines listed in Collector
 machine_ads = coll.query(
     htcondor.AdTypes.Any,
-    constraint='MyType=="Machine"',
-    projection=[
-        'Name', 'Memory', 'Disk', 'VirtualMemory',
-        'Cpus', 'Arch', 'CpuFamily', 'CpuModelNumber',
-        'TotalSlots', 'SlotType',
-        'OpSysLegacy', 'OpSysLongName', 'CondorVersion',
-        'HasMPI',
-        'HasFileTransferPluginMethods', "HasPerFileEncryption",
-        'LoadAvg', 'CondorLoadAvg', 'State', 'Activity',
-        'DaemonStartTime', 'LastHeardFrom'
-    ]
+    constraint='MyType=="Machine" && TargetType=="Job"',
+    projection=classad_keys
 )
 
 for machine in machine_ads:
-    print("SlotNumber@MachineName: {}".format(machine['Name']))
-    for (k, v) in machine.items():
-        if k != 'Name':
-            print("\t{}: {}".format(k,v))
+    print("SlotName@MachineName: {}".format(machine['Name']))
+    for key in classad_keys:
+        if key != 'Name' and key in machine:
+            print("\t{}: {}".format(key, machine[key]))
