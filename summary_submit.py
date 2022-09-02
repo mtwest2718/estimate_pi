2c715471a3e5ddc75fd3529a346e4bd0704cddc1
Matthew T West <m.t.west@exeter.ac.uk>
2022-09-02 14:39:16 +0100

    realized that transfers need to be comma separated

--- summary_submit.py
+++ summary_submit.py
@@ -1,7 +1,7 @@
 import htcondor
 import pdb
 
-sample_files = ['samples_0.csv']
+sample_files = ['samples_0.csv', 'samples_1.csv', 'samples_2.csv', 'samples_3.csv', 'samples_4.csv', 'samples_5.csv', 'samples_6.csv', 'samples_7.csv', 'samples_8.csv', 'samples_9.csv']
 outfile = 'summ_estimate.csv'
 
 summ_sub = htcondor.Submit(
@@ -9,7 +9,7 @@ summ_sub = htcondor.Submit(
     arguments = '--infiles $(infiles) --outfile $(outfile)',
     should_transfer_files = "YES",
     initialdir = 'results',
-    transfer_input_files = '$(infiles)',
+    transfer_input_files = '$(infiles_comma)',
     log = '../logs/summ.log',
     output = '../logs/summ.out',
     error = '../logs/summ.err',
@@ -17,7 +17,9 @@ summ_sub = htcondor.Submit(
     request_memory = '1GB',
     request_disk = '1GB',
 )
-summ_vars = [{'infiles': ' '.join(sample_files), 'outfile': outfile}]
+summ_vars = [{'infiles': 'samples*.csv', 'outfile': outfile, 'infiles_comma': ','.join(sample_files)}]
+
+pdb.set_trace()
 
 schedd = htcondor.Schedd()
 submit_result = schedd.submit(summ_sub, itemdata=iter(summ_vars))
